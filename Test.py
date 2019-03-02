a45d934cd37f22cdea04f61037cd3723eeecb417
burteth <ethanb127@gmail.com>
2019-03-02 11:33:40 -0500

    Added color changer

--- Test.py
+++ Test.py
@@ -23,6 +23,12 @@ from functools import partial
 root = Tk()
 root.wm_geometry("800x435")
 
+global gui_color1
+gui_color1 = '#00f3c3'
+
+global city_num
+city_num = 0
+
 self = Frame(root, background = '#222222')
 self.pack(expand=True, fill='both')
 
@@ -43,6 +49,7 @@ def callback():
     country_dict = {'AF': 'Afghanistan', 'AX': 'Åland Islands', 'AL': 'Albania', 'DZ': 'Algeria', 'AS': 'American Samoa', 'AD': 'Andorra', 'AO': 'Angola', 'AI': 'Anguilla', 'AQ': 'Antarctica', 'AG': 'Antigua and Barbuda', 'AR': 'Argentina', 'AM': 'Armenia', 'AW': 'Aruba', 'AU': 'Australia', 'AT': 'Austria', 'AZ': 'Azerbaijan', 'BS': 'Bahamas', 'BH': 'Bahrain', 'BD': 'Bangladesh', 'BB': 'Barbados', 'BY': 'Belarus', 'BE': 'Belgium', 'BZ': 'Belize', 'BJ': 'Benin', 'BM': 'Bermuda', 'BT': 'Bhutan', 'BO': 'Bolivia, Plurinational State of', 'BQ': 'Bonaire, Sint Eustatius and Saba', 'BA': 'Bosnia and Herzegovina', 'BW': 'Botswana', 'BV': 'Bouvet Island', 'BR': 'Brazil', 'IO': 'British Indian Ocean Territory', 'BN': 'Brunei Darussalam', 'BG': 'Bulgaria', 'BF': 'Burkina Faso', 'BI': 'Burundi', 'KH': 'Cambodia', 'CM': 'Cameroon', 'CA': 'Canada', 'CV': 'Cape Verde', 'KY': 'Cayman Islands', 'CF': 'Central African Republic', 'TD': 'Chad', 'CL': 'Chile', 'CN': 'China', 'CX': 'Christmas Island', 'CC': 'Cocos (Keeling) Islands', 'CO': 'Colombia', 'KM': 'Comoros', 'CG': 'Congo', 'CD': 'Congo, the Democratic Republic of the', 'CK': 'Cook Islands', 'CR': 'Costa Rica', 'CI': "Côte d'Ivoire", 'HR': 'Croatia', 'CU': 'Cuba', 'CW': 'Curaçao', 'CY': 'Cyprus', 'CZ': 'Czech Republic', 'DK': 'Denmark', 'DJ': 'Djibouti', 'DM': 'Dominica', 'DO': 'Dominican Republic', 'EC': 'Ecuador', 'EG': 'Egypt', 'SV': 'El Salvador', 'GQ': 'Equatorial Guinea', 'ER': 'Eritrea', 'EE': 'Estonia', 'ET': 'Ethiopia', 'FK': 'Falkland Islands (Malvinas)', 'FO': 'Faroe Islands', 'FJ': 'Fiji', 'FI': 'Finland', 'FR': 'France', 'GF': 'French Guiana', 'PF': 'French Polynesia', 'TF': 'French Southern Territories', 'GA': 'Gabon', 'GM': 'Gambia', 'GE': 'Georgia', 'DE': 'Germany', 'GH': 'Ghana', 'GI': 'Gibraltar', 'GR': 'Greece', 'GL': 'Greenland', 'GD': 'Grenada', 'GP': 'Guadeloupe', 'GU': 'Guam', 'GT': 'Guatemala', 'GG': 'Guernsey', 'GN': 'Guinea', 'GW': 'Guinea-Bissau', 'GY': 'Guyana', 'HT': 'Haiti', 'HM': 'Heard Island and McDonald Islands', 'VA': 'Holy See (Vatican City State)', 'HN': 'Honduras', 'HK': 'Hong Kong', 'HU': 'Hungary', 'IS': 'Iceland', 'IN': 'India', 'ID': 'Indonesia', 'IR': 'Iran, Islamic Republic of', 'IQ': 'Iraq', 'IE': 'Ireland', 'IM': 'Isle of Man', 'IL': 'Israel', 'IT': 'Italy', 'JM': 'Jamaica', 'JP': 'Japan', 'JE': 'Jersey', 'JO': 'Jordan', 'KZ': 'Kazakhstan', 'KE': 'Kenya', 'KI': 'Kiribati', 'KP': "Korea, Democratic People's Republic of", 'KR': 'Korea, Republic of', 'KW': 'Kuwait', 'KG': 'Kyrgyzstan', 'LA': "Lao People's Democratic Republic", 'LV': 'Latvia', 'LB': 'Lebanon', 'LS': 'Lesotho', 'LR': 'Liberia', 'LY': 'Libya', 'LI': 'Liechtenstein', 'LT': 'Lithuania', 'LU': 'Luxembourg', 'MO': 'Macao', 'MK': 'Macedonia, the Former Yugoslav Republic of', 'MG': 'Madagascar', 'MW': 'Malawi', 'MY': 'Malaysia', 'MV': 'Maldives', 'ML': 'Mali', 'MT': 'Malta', 'MH': 'Marshall Islands', 'MQ': 'Martinique', 'MR': 'Mauritania', 'MU': 'Mauritius', 'YT': 'Mayotte', 'MX': 'Mexico', 'FM': 'Micronesia, Federated States of', 'MD': 'Moldova, Republic of', 'MC': 'Monaco', 'MN': 'Mongolia', 'ME': 'Montenegro', 'MS': 'Montserrat', 'MA': 'Morocco', 'MZ': 'Mozambique', 'MM': 'Myanmar', 'NA': 'Namibia', 'NR': 'Nauru', 'NP': 'Nepal', 'NL': 'Netherlands', 'NC': 'New Caledonia', 'NZ': 'New Zealand', 'NI': 'Nicaragua', 'NE': 'Niger', 'NG': 'Nigeria', 'NU': 'Niue', 'NF': 'Norfolk Island', 'MP': 'Northern Mariana Islands', 'NO': 'Norway', 'OM': 'Oman', 'PK': 'Pakistan', 'PW': 'Palau', 'PS': 'Palestine, State of', 'PA': 'Panama', 'PG': 'Papua New Guinea', 'PY': 'Paraguay', 'PE': 'Peru', 'PH': 'Philippines', 'PN': 'Pitcairn', 'PL': 'Poland', 'PT': 'Portugal', 'PR': 'Puerto Rico', 'QA': 'Qatar', 'RE': 'Réunion', 'RO': 'Romania', 'RU': 'Russian Federation', 'RW': 'Rwanda', 'BL': 'Saint Barthélemy', 'SH': 'Saint Helena, Ascension and Tristan da Cunha', 'KN': 'Saint Kitts and Nevis', 'LC': 'Saint Lucia', 'MF': 'Saint Martin (French part)', 'PM': 'Saint Pierre and Miquelon', 'VC': 'Saint Vincent and the Grenadines', 'WS': 'Samoa', 'SM': 'San Marino', 'ST': 'Sao Tome and Principe', 'SA': 'Saudi Arabia', 'SN': 'Senegal', 'RS': 'Serbia', 'SC': 'Seychelles', 'SL': 'Sierra Leone', 'SG': 'Singapore', 'SX': 'Sint Maarten (Dutch part)', 'SK': 'Slovakia', 'SI': 'Slovenia', 'SB': 'Solomon Islands', 'SO': 'Somalia', 'ZA': 'South Africa', 'GS': 'South Georgia and the South Sandwich Islands', 'SS': 'South Sudan', 'ES': 'Spain', 'LK': 'Sri Lanka', 'SD': 'Sudan', 'SR': 'Suriname', 'SJ': 'Svalbard and Jan Mayen', 'SZ': 'Swaziland', 'SE': 'Sweden', 'CH': 'Switzerland', 'SY': 'Syrian Arab Republic', 'TW': 'Taiwan, Province of China', 'TJ': 'Tajikistan', 'TZ': 'Tanzania, United Republic of', 'TH': 'Thailand', 'TL': 'Timor-Leste', 'TG': 'Togo', 'TK': 'Tokelau', 'TO': 'Tonga', 'TT': 'Trinidad and Tobago', 'TN': 'Tunisia', 'TR': 'Turkey', 'TM': 'Turkmenistan', 'TC': 'Turks and Caicos Islands', 'TV': 'Tuvalu', 'UG': 'Uganda', 'UA': 'Ukraine', 'AE': 'United Arab Emirates', 'GB': 'United Kingdom', 'US': 'United States', 'UM': 'United States Minor Outlying Islands', 'UY': 'Uruguay', 'UZ': 'Uzbekistan', 'VU': 'Vanuatu', 'VE': 'Venezuela, Bolivarian Republic of', 'VN': 'Viet Nam', 'VG': 'Virgin Islands, British', 'VI': 'Virgin Islands, U.S.', 'WF': 'Wallis and Futuna', 'EH': 'Western Sahara', 'YE': 'Yemen', 'ZM': 'Zambia', 'ZW': 'Zimbabwe'}
     cities = []
     city_name = text_imput.get()
+    global country_name
     with open('/Users/ethanburt/Desktop/Coding/Weather_Python/weather-display/Weather_IDs.txt', 'rt') as city_list:
         for line in city_list:
             local_city_id = ''
@@ -70,9 +77,6 @@ def callback():
             for widget in city_info_frame.winfo_children():
                 widget.destroy()
 
-            global city_num
-            city_num = 0
-
             def information():
                 print("global value thing")
 
@@ -86,7 +90,14 @@ def callback():
                 city_num += 1
                 change_label(city_num)
 
-            range_label = Label(city_info_frame, background = '#222222', font=(settings_font, 15 ), text="1 of " + str(len(cities)), fg='#00f3c3')
+
+
+            global range_label
+            global left_arrow_label
+            global new_city_name
+            global right_arrow_label
+
+            range_label = Label(city_info_frame, background = '#222222', font=(settings_font, 15 ), text="1 of " + str(len(cities)), fg=gui_color1)
             range_label.pack(fill='y', side="top")
 
 
@@ -95,7 +106,7 @@ def callback():
             hsize = int((float(left_arrow_image.size[1]) * float(wpercent)))
             left_arrow_image = left_arrow_image.resize((30, hsize), Image.ANTIALIAS)
             left_arrow_image = ImageTk.PhotoImage(left_arrow_image)
-            left_arrow_label = Label(city_info_frame, image=left_arrow_image, background="#00f3c3")
+            left_arrow_label = Label(city_info_frame, image=left_arrow_image, background=gui_color1)
             left_arrow_label.bind("<Button-1>", move_left)
             left_arrow_label.image = left_arrow_image
             left_arrow_label.pack(side='left', expand=False)
@@ -104,10 +115,10 @@ def callback():
             detailed_city_info_frame = Frame(city_info_frame, background = '#222222')
             detailed_city_info_frame.pack(side='left', fill='y', pady=40)
 
-            new_city_name = Label(detailed_city_info_frame, background = '#222222', font=(settings_font, 15 ), text=str(cities[0][0]), fg='#00f3c3', width = 25)
+            new_city_name = Label(detailed_city_info_frame, background = '#222222', font=(settings_font, 15 ), text=str(cities[0][0]), fg=gui_color1, width = 25)
             new_city_name.pack(fill='x', padx=10)
 
-            country_name = Label(detailed_city_info_frame, background = '#222222', font=(settings_font, 15 ), text=str(cities[0][1]), fg='#00f3c3')
+            country_name = Label(detailed_city_info_frame, background = '#222222', font=(settings_font, 15 ), text=str(cities[0][1]), fg=gui_color1)
             country_name.pack(fill='x', padx=10)
 
             set_city_button = Button(detailed_city_info_frame, font=(settings_font, 15 ), text="Get Weather", fg='#222222', command=information)
@@ -122,7 +133,7 @@ def callback():
             #shift_right = partial(move_right, city_num)
 
             right_arrow_image = ImageTk.PhotoImage(right_arrow_image)
-            right_arrow_label = Label(city_info_frame, image=right_arrow_image, background="#00f3c3")
+            right_arrow_label = Label(city_info_frame, image=right_arrow_image, background=gui_color1)
             right_arrow_label.bind("<Button-1>", move_right)
             right_arrow_label.image = right_arrow_image
             right_arrow_label.pack(side='left', expand=False)
@@ -136,16 +147,37 @@ def callback():
                 range_label.config(text=(str(i + 1) + " of " + str(len(cities))))
 
 
-header_frame = Frame(self, background = '#00f3c3')
+def update_gui(new_color):
+    global gui_color1
+    gui_color1 = new_color
+    global city_num
+    if city_num != 0:
+        global range_label
+        global left_arrow_label
+        global new_city_name
+        global right_arrow_label
+        global country_name
+        range_label.config(fg=gui_color1)
+        left_arrow_label.config(bg=gui_color1)
+        new_city_name.config(fg=gui_color1)
+        country_name.config(fg=gui_color1)
+        right_arrow_label.config(bg=gui_color1)
+    header_frame.config(background=gui_color1)
+    header_label.config(background=gui_color1)
+    search_frame_label.config(fg=gui_color1)
+    color_header_label.config(fg=gui_color1)
+
+
+header_frame = Frame(self, background = gui_color1)
 header_frame.pack(fill='x', side= "top")
 
-header_label = Label(header_frame, text="Settings", fg = "#222222", bg="#00f3c3", font=(settings_font, header_size), pady=10)
+header_label = Label(header_frame, text="Settings", fg = "#222222", bg=gui_color1, font=(settings_font, header_size), pady=10)
 header_label.pack(fill="both", expand=True)
 
 left_search_frame = Frame(self, background = '#222222')
 left_search_frame.pack(fill="both", expand=False, side='left', padx=35, pady=10)
 
-search_frame_label = Label(left_search_frame, background = '#222222', font=(settings_font, header_size - 5 ), text="Change City", fg='#00f3c3', width = 20)
+search_frame_label = Label(left_search_frame, background = '#222222', font=(settings_font, header_size - 5 ), text="Change City", fg=gui_color1, width = 20)
 search_frame_label.pack(fill='x', expand=False, side="top", pady=10)
 
 search_box = Entry(left_search_frame, textvariable=text_imput, font= (settings_font, 15 ))
@@ -161,7 +193,7 @@ color_pick_frame = Frame(self, background = '#222222')
 color_pick_frame.pack(fill="both", expand=True, side='right')
 
 
-color_header_label = Label(color_pick_frame, background = '#222222', font=(settings_font, header_size - 5 ), fg='#00f3c3', width = 20, text="Change Background Colors")
+color_header_label = Label(color_pick_frame, background = '#222222', font=(settings_font, header_size - 5 ), fg=gui_color1, width = 20, text="Change Background Colors")
 color_header_label.pack(fill='x', expand=False, side="top", pady=10)
 
 def print_it(event):
@@ -189,6 +221,8 @@ def print_it(event):
             x_cord) + oval_size, int(y_cord) + oval_size, fill=color_picked, tags=("oval"))
         spectrum.create_oval((spectrum_size_width / 2) - mid_oval_size, (spectrum_size_width / 2) - mid_oval_size,
                              (spectrum_size_width / 2) + mid_oval_size, (spectrum_size_width / 2) + mid_oval_size, outline="", fill=color_picked, tags="mid_oval")
+        update_gui(color_picked)
+
 
 def draw():
     for i in range(1530):
